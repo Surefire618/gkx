5ef9ce38589460baf61251c69c4d97e2419c57fe
Surefire618 <zhaosure168@gmail.com>
2026-02-15 12:34:10 +0100

    Fix ds.attrs sigma bug

--- gkx/utils/greenkubo.py
+++ gkx/utils/greenkubo.py
@@ -283,9 +283,7 @@ def get_gk_dataset(
             keys.sigma_per_sample: (dims.time, sigma_per_sample),
         })
 
-        attrs.update({
-            keys.sigma: (dims.time, sigma),
-        })
+        attrs.update({keys.sigma: sigma})
 
         timer(msg)
 
